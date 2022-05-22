02363d448a83224ca4e74e2054106c1ae9f30e84
兰天游 <dtlantianyou@gmail.com>
2022-05-22 20:08:05 +0800

    feat: restore env variable check

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const path = require('path')
-// const {
-//   NOTION_TOKEN,
-//   BLOG_INDEX_ID,
-// } = require('./src/lib/notion/server-constants')
+const {
+  NOTION_TOKEN,
+  // BLOG_INDEX_ID,
+} = require('./src/lib/notion/server-constants')
 
 try {
   fs.unlinkSync(path.resolve('.blog_index_data'))
@@ -16,21 +16,21 @@ try {
   /* non fatal */
 }
 
-// const warnOrError =
-//   process.env.NODE_ENV !== 'production'
-//     ? console.warn
-//     : (msg) => {
-//         throw new Error(msg)
-//       }
+const warnOrError =
+  process.env.NODE_ENV !== 'production'
+    ? console.warn
+    : (msg) => {
+        throw new Error(msg)
+      }
 
-// if (!NOTION_TOKEN) {
-//   // We aren't able to build or serve images from Notion without the
-//   // NOTION_TOKEN being populated
-//   warnOrError(
-//     `\nNOTION_TOKEN is missing from env, this will result in an error\n` +
-//       `Make sure to provide one before starting Next.js`
-//   )
-// }
+if (!NOTION_TOKEN) {
+  // We aren't able to build or serve images from Notion without the
+  // NOTION_TOKEN being populated
+  warnOrError(
+    `\nNOTION_TOKEN is missing from env, this will result in an error\n` +
+      `Make sure to provide one before starting Next.js`
+  )
+}
 
 // if (!BLOG_INDEX_ID) {
 //   // We aren't able to build or serve images from Notion without the
