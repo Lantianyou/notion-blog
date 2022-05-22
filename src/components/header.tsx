c2f69cd157d09d94199656acebc33fc06750f9dd
兰天游 <dtlantianyou@gmail.com>
2022-05-22 15:46:08 +0800

    feat: remove comment

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,9 @@
-import Link from 'next/link'
 import Head from 'next/head'
-import ExtLink from './ext-link'
-import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
-
-const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'Home', page: '/' },
-  { label: 'Blog', page: '/blog' },
-  { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
-]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
 
 const Header = ({ titlePre = '' }) => {
-  const { pathname } = useRouter()
-
   return (
     <header className={styles.header}>
       <Head>
@@ -30,21 +18,6 @@ const Header = ({ titlePre = '' }) => {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={ogImageUrl} />
       </Head>
-      <ul>
-        {navItems.map(({ label, page, link }) => (
-          <li key={label}>
-            {page ? (
-              <Link href={page}>
-                <a className={pathname === page ? 'active' : undefined}>
-                  {label}
-                </a>
-              </Link>
-            ) : (
-              <ExtLink href={link}>{label}</ExtLink>
-            )}
-          </li>
-        ))}
-      </ul>
     </header>
   )
 }
